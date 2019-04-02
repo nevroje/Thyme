b7874f0410c8b53e64c1106b766d8bde98a495aa
nevroje <joakim.berglind@gmail.com>
2019-04-02 17:58:49 +0200

    added wip post api request

--- src/App.js
+++ src/App.js
@@ -138,6 +138,45 @@ class App extends Component {
     }
   }
 
+// post timesheets
+  async postTimesheets(timesheets) {
+    let apiUrl = "";
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+    
+    if (process.env.NODE_ENV === "development") {
+      apiUrl = proxyUrl + "http://demo.kimai.org/api/";
+      console.log(process.env.NODE_ENV);
+    } else {
+      apiUrl = "https://demo.kimai.org/api/";
+      console.log(process.env.NODE_ENV);
+    }
+    let headers = {
+      "X-AUTH-USER": "susan_super",
+      "X-AUTH-TOKEN": "api_kitten"
+    };
+    try {
+      const response = axios.post(apiUrl + "timesheets",
+      {
+          "begin": "2019-04-02T13:28:17.113Z",
+          "end": "2019-04-02T13:28:17.113Z",
+          "customer": 0,
+          "project": 0,
+          "activity": 0,
+          "description": "string",
+          "fixedRate": "Unknown Type: float",
+          "hourlyRate": "Unknown Type: float"
+      },
+      {
+        headers: headers
+      });
+      const timesheet = await response;
+      console.log(timesheet);
+      return timesheet;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
     return (
       <>
@@ -147,6 +186,7 @@ class App extends Component {
         <button onClick={() => this.getTime()}>Timesheets</button>
         <button onClick={() => this.getCustomers()}>Customers</button>
         <button onClick={() => this.getTranslation()}>Translations</button>
+        <button onClick={() => this.postTimesheets()}>postTimesheets</button>
       </>
     );
   }
