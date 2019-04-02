4f95588755b0804a71733a15e84e5fdd2e248776
nevroje <joakim.berglind@gmail.com>
2019-04-02 15:46:54 +0200

    added api calls for user,customer,timesheet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import axios from "axios";
 class App extends Component {
+// get pong response
   async getKimai() {
     let apiUrl = "";
     const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+    
     if (process.env.NODE_ENV === "development") {
       apiUrl = proxyUrl + "http://demo.kimai.org/api/";
       console.log(process.env.NODE_ENV);
@@ -26,12 +28,96 @@ class App extends Component {
       console.error(error);
     }
   }
+// get user data
+  async getUsers() {
+    let apiUrl = "";
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+    
+    if (process.env.NODE_ENV === "development") {
+      apiUrl = proxyUrl + "http://demo.kimai.org/api/";
+      console.log(process.env.NODE_ENV);
+    } else {
+      apiUrl = "https://demo.kimai.org/api/";
+      console.log(process.env.NODE_ENV);
+    }
+    let headers = {
+      "X-AUTH-USER": "susan_super",
+      "X-AUTH-TOKEN": "api_kitten"
+    };
+    try {
+      const response = await axios.get(apiUrl + "users", {
+        headers: headers
+      });
+      const user = await response;
+      console.log(user);
+      return user;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+// get timesheets
+  async getTime() {
+    let apiUrl = "";
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+    
+    if (process.env.NODE_ENV === "development") {
+      apiUrl = proxyUrl + "http://demo.kimai.org/api/";
+      console.log(process.env.NODE_ENV);
+    } else {
+      apiUrl = "https://demo.kimai.org/api/";
+      console.log(process.env.NODE_ENV);
+    }
+    let headers = {
+      "X-AUTH-USER": "susan_super",
+      "X-AUTH-TOKEN": "api_kitten"
+    };
+    try {
+      const response = await axios.get(apiUrl + "timesheets", {
+        headers: headers
+      });
+      const time = await response;
+      console.log(time);
+      return time;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+// get customers
+  async getCustomers() {
+    let apiUrl = "";
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+    
+    if (process.env.NODE_ENV === "development") {
+      apiUrl = proxyUrl + "http://demo.kimai.org/api/";
+      console.log(process.env.NODE_ENV);
+    } else {
+      apiUrl = "https://demo.kimai.org/api/";
+      console.log(process.env.NODE_ENV);
+    }
+    let headers = {
+      "X-AUTH-USER": "susan_super",
+      "X-AUTH-TOKEN": "api_kitten"
+    };
+    try {
+      const response = await axios.get(apiUrl + "customers", {
+        headers: headers
+      });
+      const customer = await response;
+      console.log(customer);
+      return customer;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   render() {
     return (
       <>
-        <p>hello</p>
-        <button onClick={() => this.getKimai()}>ping</button>
+        <p>View console for responses</p>
+        <button onClick={() => this.getKimai()}>Ping</button>
+        <button onClick={() => this.getUsers()}>Users</button>
+        <button onClick={() => this.getTime()}>Timesheets</button>
+        <button onClick={() => this.getCustomers()}>Cusotmers</button>
       </>
     );
   }
